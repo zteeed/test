a5f5e7335baea4be82d33181e568eb89ba306674
Slowerzs <slowerzsbot@gmail.com>
2019-11-30 19:33:56 +0100

    Fix worker's profile parsing

--- worker/worker/parser/profile.py
+++ worker/worker/parser/profile.py
@@ -8,26 +8,26 @@ from worker.parser.exceptions import RootMeParsingError
 
 def extract_pseudo(content: bytes) -> str:
     tree = html.fromstring(content)
-    result = tree.xpath('//meta[@name="author"]/@content')
+    result = tree.xpath('string(//span[@class=" forum"])')
     if not result:
         raise RootMeParsingError()
-    return unescape(result[0])
+    return unescape(result)
 
 
 def extract_score(content: bytes) -> int:
     tree = html.fromstring(content)
-    result = tree.xpath('//ul[@class="spip"]/li[3]/span/text()')
+    result = tree.xpath('string(//div[@class="small-3 columns text-center"][2]/h3)')[1:]
     if not result:  # Manage case when score is null (score is not displayed on profile)
         return 0
-    if not result[0]:  # result can be equal to [''] with this xpath search
+    if not result:  # result can be equal to '' with this xpath search
         raise RootMeParsingError()
-    result = [int(score) for score in result]
-    return result[0]
+    result = int(result)
+    return result
 
 
 def extract_avatar_url(content: bytes) -> str:
     tree = html.fromstring(content)
-    result = tree.xpath('//div[@class="t-body tb-padding"]/h1[@itemprop="givenName"]/img/@src')
+    result = tree.xpath('string(//img[@class="vmiddle logo_auteur logo_6forum"]/@src)')
     if not result:
         raise RootMeParsingError()
-    return f'{URL}/{result[0]}'
+    return f'{URL}/{result}'
